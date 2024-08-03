ce09fa459ec5a2102d4c49e211bf631585ab3772
mtalalned <muhammadtalalned@gmail.com>
2024-08-03 08:44:18 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,58 @@
+// Question No. 1
+// for (var i = 0; i < 5; i++){
+//     console.log ("Hello World")
+// }
+
+// Question No. 2
+// for (var i = 1; i <= 10; i++){
+//     console.log (i)
+// }
+
+// Question No. 3
+// var tableNumber = +prompt ('Enter number')
+// var lengt0fTable = +prompt ('Enter Length of table')
+// for (var i = 0; i <= lengt0fTable; i++){
+//     if (i === 0) {
+//         console.log ('Multiplication table of '+ tableNumber)
+//         console.log ('Length '+ lengt0fTable)
+//         continue
+//     }
+//     console.log (tableNumber + ' * ' + i + ' = ' + tableNumber * i )
+// }
+
+// Question No. 4
+// var A = ['Nokia' , 'Samsung' , 'Apple' , 'Sony' , 'Huawei']
+// for (var i = 0; i < A.length; i++){
+//     console.log (A[i])
+// }
+
+// Question No. 5
+// var A = ['apple' , 'banana' , 'mango' , 'orange' , 'strawberry']
+// for (var i = 0; i < A.length; i++){
+//     console.log (A[i])
+// }
+
+// Question No. 6
+// var lengthOfArray = +prompt ('Enter length of array')
+// var A = []
+// for ( var i = 0; i <= lengthOfArray; i++){
+//     if (i === 0){
+//         console.log ('Number of Items: '+ lengthOfArray)
+//         console.log ('Items: ')
+//         continue
+//     }
+//     var item = prompt ('Enter Item')
+//     A.push(item)
+//     console.log (A [i-1])
+// }
+
+// Question No. 7
+
+// a
+// var A = []
+// for (var i = 1; i <= 15; i++){
+//     A.push (i)
+//     if (i === 15){
+//         console.log (A.join(','))
+//     }
+// }
