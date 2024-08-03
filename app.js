e65d67a9effdfb86a6b535bb5c37968b8a009492
mtalalned <muhammadtalalned@gmail.com>
2024-08-03 11:58:54 -0700

    15 questions done

--- app.js
+++ app.js
@@ -106,11 +106,112 @@
 //     }
 // }
 
-// Question No. 9 
-// var A = [24 , 1 , 1 , 1 , 1 ]
-// for (i = 0; i < A.length; i++ ){
-//     if (A[i] > A[i+1] || A[i] > A[i-1]){
-//         var largestNumber = A[i]
+
+// Question No. 9
+// var A = [600000 , 120 , 2 , 8000000, 110 , 0.5 , 1100 , 5 , 12000 , 0.5 , 0.2 , 500000]
+// var largestNumber = A[0];
+// for (i = 0; i < A.length; i++){
+
+//     for (var j = 0; j < A.length; j++){
+    
+//         if (A[i] > A[j] && A[i] > largestNumber){
+//             largestNumber = A[i]
+//         }
+    
 //     }
 // }
 // console.log (largestNumber)
+
+
+// Question No. 10
+// var A = [600000 , 120 , 0.00000002 , 8000000, 110 , 0.5 , 1100 , 5 , 12000 , 0.5 , 0.2 , 500000]
+// var smallestNumber = A[0];
+// for (i = 0; i < A.length; i++){
+
+//     for (var j = 0; j < A.length; j++){
+    
+//         if (A[i] < A[j] && A[i] < smallestNumber){
+//             smallestNumber = A[i]
+//         }
+    
+//     }
+// }
+// console.log (smallestNumber)
+
+
+// Question No. 11
+// var A = [600000 , 120 , 0.02 , 8000000, 110 , 0.5 , 1100 , 5 , 120000000 , 0.5 , 0.003 , 500000]
+// var smallestNumber = A[0];
+// var largestNumber = A[0]
+// for (i = 0; i < A.length; i++){
+
+//     for (var j = 0; j < A.length; j++){
+    
+//         if (A[i] < A[j] && A[i] < smallestNumber){
+//             smallestNumber = A[i]
+//         }
+    
+//     }
+
+//     for (var j = 0; j < A.length; j++){
+    
+//         if (A[i] > A[j] && A[i] > largestNumber){
+//             largestNumber = A[i]
+//         }
+    
+//     }
+// }
+// console.log ('The largest number is ' + largestNumber)
+// console.log ('The smallest number is ' + smallestNumber)
+
+
+
+// Question No. 12
+// var A=[]
+// for (var i = 1; i <= 20; i++){
+//     A.push (5*i)
+//     if (i === 20){
+//         console.log (A.join(','))
+//     }
+// }
+
+
+// Question No. 13
+// var students = ['Ali' , 'Sami' , 'Taha' , 'Inam']
+// var scores = [ 58 , 73 , 89 , 90]
+// var table = document.querySelector ('table')
+
+// for (var i = 0; i < students.length && i < scores.length; i++) {
+//     if (i === 0){
+//         table.innerHTML = '<th> Students </th> <th> Scores </th>'
+//     }
+//     table.innerHTML += '<td>' + students[i] + '</td>' + '<td>' + scores[i] + '</td>'
+// }
+
+
+
+// Question No. 14
+// var scores = [12 , 45 , 3 , 22 , 34 , 50]
+// var stopValue = +prompt ('Enter value at stop')
+
+// for (var i = 0; i < scores.length; i++){
+//     if (stopValue === scores [i]){
+//         var A = scores.slice (0 , i+1)
+//         console.log (A.join (','))
+//         break
+//     }
+// }
+
+
+
+// Question No. 15
+// var A = [[ 1 , 2 , 3 ] , [ 4 , 5 , 6 ] , [ 7 , 8 , 9 ] ]
+
+// for (var i = 0; i < A.length; i++) {
+//     console.log (A[i].join (' '))
+// }
+
+
+
+
+// Question No. 16
